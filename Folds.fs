53d6474f578b25f109f71da9615f5d1a7c36c961
zoowii <1992zhouwei@gmail.com>
2018-10-24 02:09:37 +0800

    add some tranducers as lib

--- Folds.fs
+++ Folds.fs
@@ -7,7 +7,7 @@ open Microsoft.FSharp.Collections
 module Folds =
 
     type ReducingFunc<'a, 'b> = 'b -> 'a -> 'b
-    type ReducedType<'a> = Reduced of 'a | Continue of 'a
+    type ReducedType<'a> = Reduced of 'a | Continue of 'a | Ignore of 'a
     type TransducerArgs<'a, 'b> = Init | Complete of 'b | Step of 'a * 'b
     type Reducer<'a, 'b> = TransducerArgs<'a, 'b> -> ReducedType<'b>
     type Transducer<'a, 'b, 'c> = Reducer<'c, 'b> -> Reducer<'a, 'b>
@@ -26,7 +26,7 @@ module Folds =
 
         let xf (transArgs: TransducerArgs<'c, 'b>) = 
             match transArgs with
-            | Init -> Continue initial
+            | Init -> Continue initial // TODO: use transducer's init value if not provided
             | Complete acc -> Reduced acc
             | Step(input, acc) -> Continue (reducingFunc acc input)
         
@@ -41,6 +41,7 @@ module Folds =
                 match result with
                 | Reduced x -> x
                 | Continue x -> loop x
+                | Ignore x -> loop x
         loop initial
 
     let into result transducer source =
@@ -70,8 +71,12 @@ module Folds =
                     | Continue x -> 
                         yield! x
                         yield! (loop x)
+                    | Ignore x ->
+                        loop x |> ignore
             }
         loop []
+
+    let sequence = eduction // sequence now plays like eduction. TODO: cache its result
     
     let inline foldl (stepfn:'b->'a->'b)(acc:'b)(coll:#seq<'a>) : 'b = 
         use enumer = coll.GetEnumerator()
@@ -81,4 +86,68 @@ module Folds =
             | true      ->  loop ( stepfn acc' enumer.Current ) 
         loop acc 
         
-    // TODO: lazy sequence
+    let filter predictFn xf data =
+        match data with
+        | Init  -> Reduced []
+        | Complete acc -> Reduced acc
+        | Step(item, acc) -> 
+            if (predictFn item) then
+                xf (Step(item, acc))
+            else
+                Ignore acc
+
+    let map mapFn xf data =
+        match data with
+        | Init  -> Reduced []
+        | Complete acc -> Reduced acc
+        | Step(item, acc) -> 
+            xf (Step((mapFn item), acc))
+
+    let take n =
+        let mutable count = 0
+        fun xf data ->
+            match data with
+            | Init  -> Reduced []
+            | Complete acc -> Reduced acc
+            | Step(item, acc) -> 
+                if count >= n then
+                    Reduced acc
+                else
+                    count <- count + 1
+                    xf (Step(item, acc))
+
+    let skip n =
+        let mutable skippedCount = 0
+        fun xf data ->
+            match data with
+            | Init  -> Reduced []
+            | Complete acc -> Reduced acc
+            | Step(item, acc) -> 
+                if skippedCount < n then
+                    skippedCount <- skippedCount + 1
+                    Ignore acc
+                else
+                    xf (Step(item, acc))
+    let distinct<'a> =
+        let exists = HashSet<'a>()
+        fun xf data ->
+            match data with
+            | Init  -> Reduced []
+            | Complete acc -> Reduced acc
+            | Step(item, acc) -> 
+                if exists.Contains(item) then
+                    Ignore acc
+                else
+                    exists.Add(item) |> ignore
+                    xf (Step(item, acc))
+    let partition (predict: 'a -> bool) xf data =
+        match data with
+        | Init  -> Reduced ([], [])
+        | Complete acc -> Reduced acc
+        | Step(item, acc) -> 
+            match acc with
+            | (trueAcc, falseAcc) ->
+                match predict item with
+                | true -> xf (Step(item, (trueAcc @[item], falseAcc)))
+                | false -> xf (Step(item, (trueAcc, falseAcc @ [item])))
+    
--- README.md
+++ README.md
@@ -21,14 +21,23 @@ demo transducer implementation for F#
 			xf (Step(prefix + ":" + itemStr, acc))
 
 	let conj b a = [a] @ b
+    let isOdd v = (v % 2) <> 0
     let list1 = [1; 2; 3; 4; 5]
-    let a = transduce (strTransducer "hi") conj [] list1
+    let a = transduce (strTransducer "hi") conj [] list1 // ["hi:5"; "hi:4"; "hi:3"; "hi:2"; "hi:1"]
     let mystrTrans1 = strTransducer "hi1"
     let mystrTrans2 = strTransducer "hi2"
-    let b = transduce (comp mystrTrans1 mystrTrans2) conj [] list1
-    let c = eduction (strTransducer "hi") list1 // eduction returns a lazy seq
+    let b = transduce (comp mystrTrans1 mystrTrans2) conj [] list1 // ["hi2:hi1:5"; "hi2:hi1:4"; "hi2:hi1:3"; "hi2:hi1:2"; "hi2:hi1:1"]
+    let c = eduction (strTransducer "hi") list1 // eduction returns a lazy seq, c=seq("hi:1", "hi:2", "hi:3", "hi:4", "hi:5")
     Seq.iteri (fun index item ->
         printfn "c[%d]=%A" index item
     ) c
-    let d = into ["hello"] (strTransducer "hi") list1
-    printfn "a=%A, b=%A, c=%A, d=%A" a b c d
+    Seq.iteri (fun index item ->
+        printfn "c[%d]=%A" index item
+    ) c
+    let d = into ["hello"] (strTransducer "hi") list1 // ["hello"; "hi:1"; "hi:2"; "hi:3"; "hi:4"; "hi:5"]
+    let e = into [0] (filter isOdd) list1 // [0; 1; 3; 5]
+    let f = into ["map-demo"] (comp mystrTrans1 (map (fun x->"map-"+x))) list1 // ["map-demo"; "map-hi1:1"; "map-hi1:2"; "map-hi1:3"; "map-hi1:4"; "map-hi1:5"]
+    let take3 = into [] (take 3) list1 // [1; 2; 3]
+    let skip2 = into [] (skip 2) list1 // [3; 4; 5]
+    let partitionByOdd = transduce (partition isOdd) (fun pr item->pr) ([], []) list1 // ([1; 3; 5], [2; 4])
+    let distinctValues = into [] distinct [1; 2; 3; 2; 1] // [1; 2; 3]
